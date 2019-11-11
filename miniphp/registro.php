df1ae5361872a1f0655a72785b95f969937b0e4a
JuanjoAs <juanjoarevalosanchez@gmail.com>
2019-11-11 11:04:14 +0100

    Registro casi terminado

--- miniphp/registro.php
+++ miniphp/registro.php
@@ -1,24 +1,34 @@
-<?php include("includes/a_config.php");?>
+<?php include("includes/a_config.php"); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<?php include("includes/head-tag-contents.php");?>
+  <?php include("includes/head-tag-contents.php"); ?>
+  <style>
+    hr {
+      border: 0;
+      height: 1px;
+      background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));
+    }
+  </style>
 </head>
+
 <body>
 
-<?php include("includes/design-top.php");?>
-<?php include("includes/navigation.php");?>
+  <?php include("includes/design-top.php"); ?>
+  <?php include("includes/navigation.php"); ?>
 
-<main style="border:4px">
+  <main style="border:4px" class="container">
     <!--==========================
       Contact Section
     ============================-->
     <section id="contact" class="section-bg wow fadeInUp mt-5 ml-5">
-	<div class="row">
-	<div class="form col-md-12"><p> ¡BIENBENIDOS A FANBASE!</div>
-	</div>
-        <div class="row justify-content-left align-content-center">
-		
+      <div class="row">
+        <div class="form col-md-12">
+          <p> ¡BIENBENIDOS A FANBASE!
+        </div>
+      </div>
+      <div class="row justify-content-left align-content-center">
         <div class="form col-md-6">
           <div id="sendmessage">Your message has been sent. Thank you!</div>
           <div id="errormessage"></div>
@@ -46,13 +56,40 @@
             <div class="text-center"><button type="submit">Enviar Mensaje</button></div>
           </form>
         </div>
+
+        <div class="col-md-6">
+
+          <div class="row">
+            <div class="col-md-2"><img src="img/register/arcoiris.png" class="img-fluid" ></div>
+            <div class="col-md-10 ">
+              <h3 class="mt-3">¡Valora tus juegos favoritos!</h3>
+            </div>
+          </div>
+          <hr />
+          <div class="row">
+            <div class="col-md-2"><img src="img/register/arcoiris.png" class="img-fluid" ></div>
+            <div class="col-md-10 ">
+              <h3 class="mt-3">¡Comenta tus videos favoritos de tus juegos FAVORITOS!</h3>
+            </div>
+          </div>
+          <hr />
+          <div class="row">
+            <div class="col-md-2"><img src="img/register/arcoiris.png" class="img-fluid" ></div>
+            <div class="col-md-10 ">
+              <h3 class="mt-3">¡COMPARTE EL AMOR POR TODO CON LAS DEMAS PERSONAS!</h3>
+            </div>
+          </div>
+          
+
+        </div>
       </div>
     </section>
 
   </main>
-</div>
+  </div>
 
-<?php include("includes/footer.php");?>
+  <?php include("includes/footer.php"); ?>
 
 </body>
+
 </html>
