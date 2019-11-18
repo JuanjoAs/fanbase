b07505a090208906e263d217ad2a2834ac5f24e2
Antonio Escobar Rueda <escobar12996>
2019-11-18 11:17:48 +0100

    act rama

---
+++ miniphp/video.php
@@ -0,0 +1,236 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+    <head>
+	    <?php include("includes/head-tag-contents.php");?>
+    </head>
+    <body>
+    
+
+    <?php include("includes/design-top.php");?>
+    <?php include("includes/navigation.php");?>
+
+    <section id="video" class="container">
+
+<div class="row">
+    <div class="col-lg-8 col-12">
+
+        <div class="row">
+            <div class="col-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ShIbfBvNuDM"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h3>User Comment Example</h3>
+                    </div><!-- /col-sm-12 -->
+                </div><!-- /row -->
+
+
+                <!--Incio comentario-->
+                <div class="row">
+
+                    <!--Interior comentario-->
+                    <div class="col-12 p-4">
+                        <div class="row">
+                            <div class="col-1">
+                                <div class="card">
+                                    <img src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                                        class="card-img-top" title="usename">
+                                </div>
+                            </div>
+
+                            <div class="col-11">
+                                <div class="row">
+                                    username
+                                </div>
+                                <div class="row">
+                                    Lorem Ipsum is simply dummy text of the printing and
+                                    typesetting industry. Lorem Ipsum has been the industry's
+                                    standard dummy text ever since the 1500s, when an unknown...
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Incio comentario-->
+                <div class="row">
+
+                    <!--Interior comentario-->
+                    <div class="col-12 p-4">
+
+                        <div class="row">
+                            <div class="col-1">
+                                <div class="card">
+                                    <img src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                                        class="card-img-top" title="usename">
+                                </div>
+                            </div>
+
+                            <div class="col-11">
+                                <div class="row">
+                                    username
+                                </div>
+                                <div class="row">
+                                    Lorem Ipsum is simply dummy text of the printing and
+                                    typesetting industry. Lorem Ipsum has been the industry's
+                                    standard dummy text ever since the 1500s, when an unknown
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-12">
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/PlanetZoo.png" class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                Tráiler Planet Zoo
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/Primer tráiler gameplay de Overwatch 2 (PC, Mac, PS4, XOne).png"
+                        class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                Primer Tráiler Overwatch 2
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/Riverbond fecha con un tráiler su lanzamiento en Nintendo Switch y presenta nuevo mundo (PC, PS.png"
+                        class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                Tráiler Riverbond
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/Todo lo que has de saber de Pokémon Espada y Pokémon.png" class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                    Todo lo que has de saber de Pokemon
+            </div>
+        </div>
+
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/TrailerKickstarterdeTheHive.png" class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                    Tráiler Kickstarted The Hive
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/TrailerPersona5Scramble.png" class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                    Tráiler Persona 5 Scramble
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/Tráiler cinemático de anuncio de Diablo.png" class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                    Tráiler Cinematográfico Diablo 4
+            </div>
+        </div>
+
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/Tráiler cinemático de anuncio World of Warcraft Shadowlands.png" class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                    Tráiler cinemático World of Warcraft Shadowlands
+
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/Tráiler de anuncio de IGI Origins.png" class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                    Trailer IGI Origins
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/Tráiler de anuncio de Vaporum Lockdown, un dungeon crawler con estética steampunk.png" class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                    Tráiler de Vaporum Lockdown
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <div class="row">
+                    <img src="img/trailer/Tráiler de Soul Calibur VI que presenta a Hilde, su nueva luchadora.png" class="img-fluid" title="usename">
+                </div>
+            </div>
+            <div class="col-7">
+                    Tráiler de Soul Calibur VI que presenta a Hilde
+            </div>
+        </div>
+
+       
+
+
+
+    </div>
+</div>
+</section>
+
+   
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
