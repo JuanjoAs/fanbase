bf66592bca29adb220777ed07668c1289d6e7ded
rserrano96 <rafa.serrano1996@gmail.com>
2019-11-26 14:31:14 +0100

    juego

--- miniphp/info_juego.php
+++ miniphp/info_juego.php
@@ -83,6 +83,34 @@
                     <span class="sr-only">Next</span>
                 </a>
             </div>
+
+            <div class="row mb-3 justify-content-around">
+                <div class="card col-lg-3 col-md-6">
+                    <img class="card-img-top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-3 col-md-6">
+                    <img class="card-img-top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/008.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-lg-3 col-md-6">
+                    <img class="card-img-top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
         <?php } ?>
     </section>
   </main>
