726b5df7d003d7d4a691532ba226ebbac0f5b20b
Antonio Escobar Rueda <escobar12996>
2019-12-02 11:14:40 +0100

    actualizacion

---
+++ miniphp/trailers.php
@@ -0,0 +1,173 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include("includes/head-tag-contents.php");?>
+</head>
+
+<body>
+
+    <?php include("includes/design-top.php");?>
+    <?php include("includes/navbar.php");?>
+
+    <main class="">
+        <section class="section-bg wow fadeInUp trailers">
+            <div class="container">
+                <div class="">
+                    <div class="section-header">
+                        <h2>Tráilers</h2>
+                        <p></p>
+                    </div>
+                    <div class="row">
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3">
+                            <a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/PlanetZoo.png" class="card-img-top h-100"
+                                        alt="Tráiler Planet Zoo" title="Tráiler Planet Zoo">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Tráiler Planet Zoo</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/Primer tráiler gameplay de Overwatch 2 (PC, Mac, PS4, XOne).png"
+                                        class="card-img-top h-100" alt="Tráiler de Overwatch 2"
+                                        title="Tráiler de Overwatch 2">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Tráiler de Overwatch 2</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/Riverbond fecha con un tráiler su lanzamiento en Nintendo Switch y presenta nuevo mundo (PC, PS.png"
+                                        class="card-img-top h-100"
+                                        alt="Riverbond tráiler lanzamiento en Nintendo Switch"
+                                        title="Riverbond tráiler lanzamiento en Nintendo Switch">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Riverbond tráiler lanzamiento en Nintendo Switch</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/Todo lo que has de saber de Pokémon Espada y Pokémon.png"
+                                        class="card-img-top h-100"
+                                        alt="Todo lo que has de saber de Pokémon Espada y Pokémon"
+                                        title="Todo lo que has de saber de Pokémon Espada y Pokémon">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Todo lo que has de saber de Pokémon Espada y Pokémon</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/Tráiler cinemático de anuncio de Diablo.png"
+                                        class="card-img-top h-100"
+                                        alt="Tráiler cinemático de anuncio de Diablo"
+                                        title="Tráiler cinemático de anuncio de Diablo">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Tráiler cinemático de anuncio de Diablo</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/Tráiler cinemático de anuncio World of Warcraft Shadowlands.png"
+                                        class="card-img-top h-100"
+                                        alt="Tráiler cinemático de anuncio World of Warcraft Shadowlands"
+                                        title="Tráiler cinemático de anuncio World of Warcraft Shadowlands">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Tráiler cinemático de anuncio World of Warcraft
+                                            Shadowlands</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/Tráiler de anuncio de IGI Origins.png"
+                                        class="card-img-top h-100" alt="Tráiler de anuncio de IGI Origins"
+                                        title="Tráiler de anuncio de IGI Origins">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Tráiler de anuncio de IGI Origins</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/Tráiler de anuncio de Vaporum Lockdown, un dungeon crawler con estética steampunk.png"
+                                        class="card-img-top h-100"
+                                        alt="Tráiler de anuncio de Vaporum Lockdown, un dungeon crawler con estética steampunk"
+                                        title="Tráiler de anuncio de Vaporum Lockdown, un dungeon crawler con estética steampunk">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Tráiler de anuncio de Vaporum Lockdown</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/Tráiler de Soul Calibur VI que presenta a Hilde, su nueva luchadora.png"
+                                        class="card-img-top h-100"
+                                        alt="Tráiler de Soul Calibur VI que presenta a Hilde, su nueva luchadora"
+                                        title="Tráiler de Soul Calibur VI que presenta a Hilde, su nueva luchadora">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Tráiler de Soul Calibur VI que presenta a Hilde, su nueva
+                                            luchadora</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/TrailerKickstarterdeTheHive.png"
+                                        class="card-img-top h-100" alt="Trailer Kickstarter de The Hive"
+                                        title="Trailer Kickstarter de The Hive">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Trailer Kickstarter de The Hive</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/TrailerPersona5Scramble.png"
+                                        class="card-img-top h-100" alt="Trailer Persona 5 Scramble"
+                                        title="Trailer Persona 5 Scramble">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Trailer Persona 5 Scramble</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-3"><a href="video.php">
+                                <div class="card tarjeta-trailers">
+                                    <img src="assets/img/trailer/Ya tenemos tráiler de lanzamiento en español para Star Wars Jedi Fallen Order.png"
+                                        class="card-img-top h-100"
+                                        alt="Tráiler español para Star Wars Jedi Fallen Order"
+                                        title="Tráiler español para Star Wars Jedi Fallen Order">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Tráiler español para Star Wars Jedi Fallen Order</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+<?php include("includes/footer.php");?>
+
+</body>
+
+</html>
