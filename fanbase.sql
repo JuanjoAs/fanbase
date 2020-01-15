7f8456e218dc6099afed3232c3722a9607ea8c11
rserrano96 <rafa.serrano1996@gmail.com>
2020-01-15 14:21:06 +0100

    update bd

--- fanbase.sql
+++ fanbase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-01-2020 a las 11:23:13
+-- Tiempo de generación: 15-01-2020 a las 14:16:56
 -- Versión del servidor: 10.4.10-MariaDB
 -- Versión de PHP: 7.3.12
 
@@ -25,10 +25,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `recomendaciones`
+-- Estructura de tabla para la tabla `recomendacion`
 --
 
-CREATE TABLE `recomendaciones` (
+CREATE TABLE `recomendacion` (
   `nombre` varchar(55) COLLATE utf8_spanish_ci NOT NULL,
   `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
   `imagen` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
@@ -44,10 +44,10 @@ CREATE TABLE `recomendaciones` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `recomendaciones`
+-- Volcado de datos para la tabla `recomendacion`
 --
 
-INSERT INTO `recomendaciones` (`nombre`, `descripcion`, `imagen`, `tipo`, `plataforma1`, `plataforma2`, `plataforma3`, `plataforma4`, `linkplataforma1`, `linkplataforma2`, `linkplataforma3`, `linkplataforma4`) VALUES
+INSERT INTO `recomendacion` (`nombre`, `descripcion`, `imagen`, `tipo`, `plataforma1`, `plataforma2`, `plataforma3`, `plataforma4`, `linkplataforma1`, `linkplataforma2`, `linkplataforma3`, `linkplataforma4`) VALUES
 ('Ataque a los Titanes', 'Shingeki no Kyojin (進撃の巨人?), también conocida en países de habla hispana como Ataque a los titanes1​ y Ataque de los titanes,2​ es una serie de manga escrita e ilustrada por Hajime Isayama. El manga se publicó por primera vez en septiembre de 2009 en la revista Bessatsu Shōnen Magazine.<br>\r\n\r\nLa trama gira en torno a Eren Jaeger quien junto a sus amigos de la infancia, decide unirse al ejército con el objetivo de vengar la muerte de su madre y la destrucción de su ciudad a manos de unos seres humanoides llamadas Titanes. Más adelante, Eren descubrirá que posee la capacidad de transformarse en un Titán, lo que desencadena toda una serie de acontecimientos que afectarán tanto a Eren, como a todos los que le rodean.', 'assets/img/recomendaciones/titanes.png', 'seriepeli', 'imdb', '', '', '', 'https://www.imdb.com/title/tt2560140/', '', '', ''),
 ('Cyberpunk 2077', 'El juego AAA más esperado de la última década. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate tempore ea sit commodi. At nisi maiores quo doloribus aperiam quam nam fuga eaque a tempore excepturi nemo hic, quod voluptate. At nisi maiores quo doloribus aperiam quam nam fuga eaque a tempore excepturi nemo hic, quod voluptate.\r\n', 'assets/img/recomendaciones/cyberpunk.png', 'juego', 'gog', 'steam', 'microsoft', '', 'https://www.gog.com/game/cyberpunk_2077', 'https://store.steampowered.com/app/1091500/Cyberpunk_2077/', 'https://www.microsoft.com/es-es/p/cyberpunk-2077/bx3m8l83bbrw', ''),
 ('El Señor de los Anillos: El retorno del rey', 'El Señor de los Anillos: el retorno del Rey (título original en inglés: The Lord of the Rings: The Return of the King) es la tercera película de la trilogía cinematográfica de El Señor de los Anillos, dirigida por Peter Jackson y basada en la tercera parte de la obra de J. R. R. Tolkien, El Señor de los Anillos. Tuvo un presupuesto de 94 millones de dólares y fue rodada del 11 de octubre de 1999 al 22 de diciembre de 2000.\r\n<br>\r\nTrata sobre la última parte del viaje que emprendieron los nueve compañeros (de los cuales quedan solamente ocho) para salvar a la Tierra Media de la oscuridad impuesta por Sauron. En esta parte se decide el destino de todos los habitantes de estas tierras.\r\n<br>\r\nLos primeros dos filmes fueron El Señor de los Anillos: la Comunidad del Anillo y El Señor de los Anillos: las dos torres, aunque en esta película se incluyen algunos eventos del libro anterior: Las dos torres.', 'assets/img/recomendaciones/anillos3.png', 'seriepeli', 'imdb', '', '', '', 'https://www.imdb.com/title/tt0167260/', '', '', ''),
@@ -58,35 +58,35 @@ INSERT INTO `recomendaciones` (`nombre`, `descripcion`, `imagen`, `tipo`, `plata
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `usuarios` (
+CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `usuario` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `email` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `pasword` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `rango` set('admin','user','editor','') COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `nombre`, `email`, `pasword`, `rango`) VALUES
+INSERT INTO `usuario` (`id`, `usuario`, `nombre`, `email`, `password`, `rango`) VALUES
 (1, 'admin', 'SysAdmin', 'admin@fanbase.com', '1234', 'admin');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `valoraciones`
+-- Estructura de tabla para la tabla `valoracion`
 --
 
-CREATE TABLE `valoraciones` (
+CREATE TABLE `valoracion` (
   `id` int(11) NOT NULL,
-  `idVideo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `idUsuario` int(11) NOT NULL,
+  `id_video` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `comentario` text COLLATE utf8_spanish_ci NOT NULL,
   `valoracion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
@@ -96,40 +96,51 @@ CREATE TABLE `valoraciones` (
 --
 
 --
--- Indices de la tabla `recomendaciones`
+-- Indices de la tabla `recomendacion`
 --
-ALTER TABLE `recomendaciones`
+ALTER TABLE `recomendacion`
   ADD PRIMARY KEY (`nombre`);
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `usuario`
 --
-ALTER TABLE `usuarios`
+ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `usuario` (`usuario`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indices de la tabla `valoraciones`
+-- Indices de la tabla `valoracion`
 --
-ALTER TABLE `valoraciones`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `valoracion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT de la tabla `usuario`
 --
-ALTER TABLE `usuarios`
+ALTER TABLE `usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `valoraciones`
+-- AUTO_INCREMENT de la tabla `valoracion`
 --
-ALTER TABLE `valoraciones`
+ALTER TABLE `valoracion`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `valoracion`
+--
+ALTER TABLE `valoracion`
+  ADD CONSTRAINT `valoracion_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
