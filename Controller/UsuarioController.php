c2a595af25a8a1167db3fe97db2bcbd625039801
rserrano96 <rafa.serrano1996@gmail.com>
2020-01-20 10:40:23 +0100

    usuariocontroller

--- Controller/UsuarioController.php
+++ Controller/UsuarioController.php
@@ -17,8 +17,9 @@ class UsuarioController {
         $usuarios = $c->query($sql);
 
         while($obj = $usuarios->fetch(PDO::FETCH_OBJ)) {
-            $u = new Usuario($obj->id, $obj->usuario, $obj->nombre,
+            $u = new Usuario($obj->usuario, $obj->nombre,
                 $obj->email, $obj->password, $obj->rango);
+            $u->id = $obj->usuario;
             $coleccion[] = $u;
         }
 
@@ -27,6 +28,55 @@ class UsuarioController {
         return $coleccion;
     }
 
+    public static function find($cod) {
+        $error = false;
 
+        try {
+            $c = new Conexion();
+            $c->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $ex) {
+            die('Error fatal, imposible conectar con la base de datos.');
+        }
+
+        $sql = "SELECT * FROM usuario WHERE id='$cod'";
+
+        try {
+            $usuario = $c->query($sql);
+        } catch (Exception $ex) {
+            $error = true;
+        }
+
+        unset($c);
+
+        if(!$error && $usuario->rowCount()) {
+            $obj = $usuario->fetch(PDO::FETCH_OBJ);
+            return new Usuario($obj->id, $obj->usuario, $obj->nombre,
+                $obj->email, $obj->password, $obj->rango);
+        }
+
+        return false;
+    }
+
+    public static function insert($usuario) {
+        $success = true;
+
+        try {
+            $c = new Conexion();
+            $c->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $ex) {
+            die('Error fatal, imposible conectar con la base de datos.');
+        }
+
+        $sql = "INSERT INTO usuario (usuario, nombre, email, password, rango) VALUES('$usuario->usuario', '$usuario->nombre', '$usuario->email',"
+            . "$usuario->password, '$usuario->rango')";
+
+        try {
+            $query = $c->query($sql);
+        } catch(PDOException $ex) {
+            $success = false;
+        }
+
+        return $success;
+    }
 
 }
