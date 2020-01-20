22885ebcd9cfdb70f333c4764b12faf8ab6a74e1
Francisco Trillo <fr.trillo7@gmail.com>
2020-01-20 11:22:49 +0100

    scss sass panel backend recomendaciones

--- panelrecomendaciones.php
+++ panelrecomendaciones.php
@@ -1,76 +1,93 @@
-<?php 
+<?php
 include("includes/a_config.php");
 require_once 'Model/Recomendacion.php';
-
+$PAGE_TITLE = "FanBase - Backend Recomendaciones";
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-	<?php include("includes/head-tag-contents.php");?>
+  <?php include("includes/head-tag-contents.php"); ?>
 </head>
+
 <body>
 
-<?php include("includes/navbar.php");?>
+  <?php include("includes/navbar.php"); ?>
 
-<main class="container">
+  <main class="container">
 
-<!--==========================
+    <!--==========================
       Backend recomendaciones
     ============================-->
     <section class="wow fadeInUp recomendaciones">
-        <div class="section-header">
-          <h2>Administrar Juegos</h2>
-        </div>
-        <div class="justify-content-center">
+      <div class="section-header">
+        <h2>Administrar Juegos</h2>
+      </div>
+      <div class="justify-content-center">
         <ul class="recomendaciones-list">
-                <?php
-                $recomendaciones=RecomendacionObj::recuperarTodosJuegos();
-                ?>
-                <?php
-                $i=-1;
-                foreach($recomendaciones as $recomendacion){
-                  $i++;
-                ?>
-                  <li class="border">
-                    <div id="juegos<?php echo $i;?>">
-                      <div class="row">
-                        <div class="col-lg-8">
-                          <a class="mt-3"><?php echo $recomendacion->nombre; ?></a>                
-                        </div>
-                        <div class="col-lg-2">
-                        <button class="btn btn-primary mt-2 mb-2">Editar</button>
-                        </div>
-                        <div class="col-lg-2">
-                          <div class="row">
-                            <button class="btn btn-danger m-2">Borrar</button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                <?php
-                }
-                ?>
-                
-              </ul>
+          <?php
+          $recomendaciones = RecomendacionObj::recuperarTodosJuegos();
+          ?>
+          <?php
+          $i = -1;
+          foreach ($recomendaciones as $recomendacion) {
+            $i++;
+          ?>
+            <li class="border">
+              <div id="juegos<?php echo $i; ?>">
+                <div class="row">
+                  <div class="col-lg-9">
+                    <span class="mt-3"><?php echo $recomendacion->nombre; ?></span>
+                  </div>
+                  <div class="col-lg-3">
+                    <button class="btn btn-success m-2">Editar</button>
+                    <button class="btn btn-danger m-2">Borrar</button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          <?php
+          }
+          ?>
+        </ul>
+      </div>
         <div class="section-header">
           <h2>Administrar Series</h2>
         </div>
         <div class="justify-content-center">
-        <div class="row border">
-          <div class="col-lg-9">
-            Nombre serie
-          </div>
-          <div class="col-lg-3">
-            <button class="btn btn-primary">Editar</button>
-            <button class="btn btn-danger">Borrar</button>
-          </div>
-        </div>
-        </div>
+        <ul class="recomendaciones-list">
+          <?php
+          $recomendaciones = RecomendacionObj::recuperarTodosSeries();
+          ?>
+          <?php
+          $i = -1;
+          foreach ($recomendaciones as $recomendacion) {
+            $i++;
+          ?>
+            <li class="border">
+              <div id="juegos<?php echo $i; ?>">
+                <div class="row">
+                  <div class="col-lg-9">
+                    <span class="mt-3"><?php echo $recomendacion->nombre; ?></span>
+                  </div>
+                  <div class="col-lg-3">
+                    <button class="btn btn-success m-2">Editar</button>
+                    <button class="btn btn-danger m-2">Borrar</button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          <?php
+          }
+          ?>
+        </ul>
+      </div>
+
     </section>
 
-</main>
+  </main>
 
-<?php include("includes/footer.php");?>  
+  <?php include("includes/footer.php"); ?>
 </body>
+
 </html>
