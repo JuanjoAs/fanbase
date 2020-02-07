9a289e3329ae156f484103764811ba56cba22ba5
Francisco Trillo <fr.trillo7@gmail.com>
2020-02-07 13:59:38 +0100

    quill customizado

--- includes/quilleditor.php
+++ includes/quilleditor.php
@@ -2,6 +2,27 @@
     #editor-container {
         height: 150px;
     }
+
+    #toolbar-container .ql-font span[data-label="Permanent Marker"]::before {
+        font-family: "Permanent Marker";
+    }
+
+    #toolbar-container .ql-font span[data-label="permanent marker"]::before {
+        font-family: "permanent marker";
+    }
+
+    #toolbar-container .ql-font span[data-label="Roboto"]::before {
+        font-family: "Roboto";
+    }
+
+    /* Set content font-families */
+    .ql-font-permanentmarker {
+        font-family: "permanent marker";
+    }
+
+    .ql-font-roboto {
+        font-family: "Roboto";
+    }
 </style>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
@@ -11,7 +32,5 @@
 <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
 
 <!-- Theme included stylesheets -->
-<link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+<link href="assets/css/fanbase.snow.css" rel="stylesheet">
 <link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
-
-
